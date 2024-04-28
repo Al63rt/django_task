431c8467fb1025d3bd1275d93de5d61ac95834b5
Al63rt <168301845+Al63rt@users.noreply.github.com>
2024-04-29 00:18:56 +0300

    After 20

--- project_tracker/tasks/urls.py
+++ project_tracker/tasks/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from tasks import views
 
 app_name = 'tasks'
@@ -14,4 +14,9 @@ urlpatterns = [
     path('projects/', views.ProjectsListView.as_view(), name='projects_list'),
     path('projects/<int:project_id>/', views.ProjectDetailView.as_view(), name='project_detail'),
     path('projects/<int:project_id>/tasks/<int:task_id>/', views.TaskDetailView.as_view(), name='task_detail'),
+
+    # ... другие URL-маршруты ...
+    path('feedback/', views.feedback_view, name='feedback'),
+    path('project/new/', views.create_project, name='create_project'),
+    path('project/<int:project_id>/add_task/', views.add_task_to_project, name='add_task_to_project'),
 ]
--- project_tracker/tasks/views.py
+++ project_tracker/tasks/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponse
-from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import Project, Task
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm, ProjectForm, TaskForm
+from django.core.mail import send_mail
 
 #Function Based Views
+
 def index(request):
     return render(request, 'tasks/index.html')
 
@@ -27,6 +28,7 @@ class IndexView(View):
         return render(request, 'tasks/index.html')
 
 #Class Based Views
+
 class ProjectsListView(ListView):
     model = Project
     template_name = 'tasks/projects_list.html'
@@ -40,3 +42,45 @@ class TaskDetailView(DetailView):
     model = Task
     pk_url_kwarg = 'task_id'
     template_name = 'tasks/task_detail.html'
+
+
+def feedback_view(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            
+            recipients = ['info@example.com']
+            recipients.append(email)
+            
+            send_mail(name, message, email, recipients)
+            
+            return redirect('/tasks')
+    else:
+        form = FeedbackForm()
+    return render(request, 'tasks/feedback.html', {'form': form})
+
+def create_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:projects_list')
+    else:
+        form = ProjectForm()
+    return render(request, 'tasks/project_create.html', {'form': form})
+
+def add_task_to_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.project = project
+            task.save()
+            return redirect('tasks:project_detail', project_id=project.id)
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form, 'project': project})
